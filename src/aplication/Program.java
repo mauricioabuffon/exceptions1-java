9645ce99ed6ae728d339e7f22a66b59cf1022d5a
Mauricio Antonio Buffon <mauricioabuffon@gmail.com>
2020-11-05 14:23:52 -0300

    Good solution

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -6,23 +6,22 @@ import java.util.Date;
 import java.util.Scanner;
 
 import model.entities.Reservation;
+import model.exceptions.DomainException;
 
 public class Program {
 
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-		System.out.println("Room number: ");
-		int number = sc.nextInt();
-		System.out.print("CheckIn date: ");
-		Date checkIn = sdf.parse(sc.next());
-		System.out.print("CheckOut date: ");
-		Date checkOut = sdf.parse(sc.next());
-		if (!checkOut.after(checkIn)) {
-			System.out.println("Inconsistency into dates");
-		} else {
+		try {
+			System.out.println("Room number: ");
+			int number = sc.nextInt();
+			System.out.print("CheckIn date: ");
+			Date checkIn = sdf.parse(sc.next());
+			System.out.print("CheckOut date: ");
+			Date checkOut = sdf.parse(sc.next());
 			Reservation reservation = new Reservation(number, checkIn, checkOut);
 			System.out.println("Reservation: " + reservation);
 
@@ -33,15 +32,16 @@ public class Program {
 			System.out.print("CheckOut date: ");
 			checkOut = sdf.parse(sc.next());
 
-			String error = reservation.updateDates(checkIn, checkOut);
-			if (error != null) {
-				System.out.println("Error in Reservation: " + error + " in reservation: " + sdf.format(checkIn) + " , " + sdf.format(checkOut));
-			} else {
-				System.out.println("Reservation: " + reservation);
-
-			}
+			reservation.updateDates(checkIn, checkOut);
+			System.out.println("Reservation: " + reservation);
+		} catch (ParseException e) {
+			System.out.println("Invalid date format");
+		} catch (DomainException e) {
+			System.out.println("Error in reservation" + e.getMessage());
 		}
-
+		catch(RuntimeException e) {
+			System.out.println("Run time erro: " + e.getMessage());
+		}
 		sc.close();
 	}
 }
--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -4,6 +4,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import model.exceptions.DomainException;
+
 public class Reservation {
 
 	private Integer roomNumber;
@@ -16,7 +18,10 @@ public class Reservation {
 
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+	public Reservation(Integer roomNumber, Date checkIn, Date checkOut)  {
+		if (!checkOut.after(checkIn)) {
+			throw new DomainException("Inconsistency into dates checkOut checkIn");
+		}
 		this.roomNumber = roomNumber;
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
@@ -43,17 +48,16 @@ public class Reservation {
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
 
-	public String updateDates(Date checkIn, Date checkOut) {
+	public void updateDates(Date checkIn, Date checkOut)  {
 		Date now = new Date();
 		if (checkIn.before(now) || checkOut.before(now)) {
-			return "Error - dates need to be after actual";
+			throw new DomainException("Error - reservation dates need to be after actual");
 		}
 		if (!checkOut.after(checkIn)) {
-			return "Inconsistency into dates checkOut checkIn";
+			throw new DomainException("Inconsistency into dates checkOut checkIn");
 		}
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
-		return null;
 	}
 
 	public String toString() {
